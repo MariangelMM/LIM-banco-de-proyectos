834e6eedd8bf0cd2a26de7ecd0e168694614026f
MariangelMM <mariangelmoramarquez@gmail.com>
2020-05-12 01:26:02 -0500

    Updates

---
+++ precache-manifest.2b29bb05822e61744959c518f0f77990.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a73067d0df52e0bdca7ef468a2588fd",
+    "url": "/eMusic/index.html"
+  },
+  {
+    "revision": "d36a1f135dfbdd323eff",
+    "url": "/eMusic/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "3d011b423bf0c7f7c66b",
+    "url": "/eMusic/static/css/main.f72e8ef2.chunk.css"
+  },
+  {
+    "revision": "d36a1f135dfbdd323eff",
+    "url": "/eMusic/static/js/2.d10be3ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eMusic/static/js/2.d10be3ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d011b423bf0c7f7c66b",
+    "url": "/eMusic/static/js/main.4652c951.chunk.js"
+  },
+  {
+    "revision": "5fa4d830574b85786fe9",
+    "url": "/eMusic/static/js/runtime-main.bfe74dba.js"
+  },
+  {
+    "revision": "cc590710ebacbaa42408ea922456ee9e",
+    "url": "/eMusic/static/media/imagen.cc590710.jpg"
+  }
+]);
